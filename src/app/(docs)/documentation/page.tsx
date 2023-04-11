706b3f21257e3a75e66b73738c73b8d70598d0b4
Fehlling Halim <kevnhalim@gmail.com>
2023-04-11 14:23:48 +0700

    feat: add documentation page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+
+import { Metadata } from "next";
+import Heading1 from "@/components/ui/Heading1";
+import Paragraph from "@/components/ui/Paragraph";
+import DocsTab from "@/components/DocsTab";
+
+export const metadata: Metadata = {
+    title: "API Server | Documentation",
+    description: "Open-source API usable by everyone",
+};
+
+const page: FC = () => {
+    return (
+        <div className="container max-w-7xl mx-auto mt-12">
+            <div className="flex flex-col items-center gap-6">
+                <Heading1>Creating a request</Heading1>
+                <Paragraph>api/v1/apiserver</Paragraph>
+
+                <DocsTab />
+            </div>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/components/DocsTab.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const DocsTab: FC = () => {
+    return <div></div>;
+};
+
+export default DocsTab;
